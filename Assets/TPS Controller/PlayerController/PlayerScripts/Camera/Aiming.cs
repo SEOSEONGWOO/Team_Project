34bdfe382a366a89a83d3e23786bebb3a0bcdf7e
SEOSEONGWOO <jsn0526@naver.com>
2021-04-05 18:38:45 +0900

    에셋추가

---
+++ Assets/TPS Controller/PlayerController/PlayerScripts/Camera/Aiming.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aiming : MonoBehaviour {
+
+	//Camera zoom control
+	[Range(10,60)]
+	public int zoom = 20; //Customizable zoom
+	[Header("Scope texture")]
+	public Texture2D crosshair;
+	[Header("Sight size in height")]
+	public int height = 40;
+	[Header("Sight size in width")]
+	public int width = 40;
+
+	private int smoothZoom = 10; //Smooth zoom change
+	private int normal = 60; //Normal field of view of the camera
+	private float isZoomed = 0; //Aiming is enabled by default
+
+	void Update()
+	{
+		if (Input.GetMouseButton (1)) //Right click on the right mouse button
+		{
+	
+			isZoomed = 1;  //isZoomed becomes active
+		}
+		if (isZoomed == 1) {
+			GetComponent<Camera> ().fieldOfView = Mathf.Lerp (GetComponent<Camera> ().fieldOfView, zoom, Time.deltaTime * smoothZoom); //Ищим камеру на нашем объекте и меняем поле зрения
+		}
+		if (Input.GetMouseButtonUp (1))
+		{
+
+			isZoomed = 0;
+		}
+		if(isZoomed == 0)
+		{
+			GetComponent<Camera> ().fieldOfView = Mathf.Lerp (GetComponent<Camera> ().fieldOfView, normal, Time.deltaTime * smoothZoom); //Если зуум не активен, то возвращаем значение поля зрения по умолчанию
+		}
+	}
+}
---
+++ Assets/TPS Controller/PlayerController/PlayerScripts/ShowFPS.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour {
+	
+		private const float UpdateInterval = 1.0f;
+		private float _timeleft;
+		private float _lastTime;
+		private float _timeSpan;
+		private int _lastFrame;
+		private int _frames;
+		private float _fps;
+
+		void Update()
+		{
+			_timeleft += Time.deltaTime;
+			if (_timeleft > UpdateInterval)
+			{
+				_timeleft -= UpdateInterval;
+				_frames = Time.frameCount - _lastFrame;
+				_lastFrame = Time.frameCount;
+				_timeSpan = Time.realtimeSinceStartup - _lastTime;
+				_lastTime = Time.realtimeSinceStartup;
+				_fps = Mathf.RoundToInt(_frames/_timeSpan);
+			}
+		}
+
+		void OnGUI()
+		{
+			GUI.Box(new Rect(10, 10, 70, 25), string.Format("fps {0}", _fps));
+		}
+	}
