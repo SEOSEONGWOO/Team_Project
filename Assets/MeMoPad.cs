313d53bf83a12b233f640642183c6236a8316bc7
jhb <tmvlfflrm@gmail.com>
2021-04-06 17:23:10 +0900

    Hong

--- Assets/MeMoPad.cs
+++ Assets/MeMoPad.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MeMoPad : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -16,3 +16,6 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 }
+
+
+ 2021-04-06 우리 다 같이 화이팅해봐요 ~~  !! ^^ 
