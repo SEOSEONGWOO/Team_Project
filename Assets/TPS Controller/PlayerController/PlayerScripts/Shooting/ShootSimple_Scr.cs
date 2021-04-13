d55eff15c6c0c9452af351a5f31b9a240992ea09
SEOSEONGWOO <jsn0526@naver.com>
2021-04-13 15:53:28 +0900

    sda

--- Assets/TPS Controller/PlayerController/PlayerScripts/Shooting/ShootSimple_Scr.cs
+++ Assets/TPS Controller/PlayerController/PlayerScripts/Shooting/ShootSimple_Scr.cs
@@ -13,9 +13,12 @@ public class ShootSimple_Scr : MonoBehaviour
 	public Transform targetLook;
 	[Header("Bullet")]
     public GameObject bullet;
+    public GameObject skill1_bullet;
 
     [Header("If true, then you can shoot")]
-    public bool isshoot = false;
+    public bool isshoot = false;  //기본 공격 제어
+    public bool isSkill1 = false;  //스킬1 제어
+
 
     [Header("Bullet flight distance")]
     public float distance;
@@ -49,31 +52,74 @@ public class ShootSimple_Scr : MonoBehaviour
     {
         weaponPoint.LookAt(targetLook);
         aimPoint.LookAt(targetLook);
-        if ((Input.GetMouseButton(0))&&(isshoot == true)) {
-            isshoot = false;
-			audioSource.PlayOneShot (fireSound);
+        Attack();
+        skill1();
+
+
+
+    }
+
+
+     void Attack()
+        {
+            if ((Input.GetMouseButton(0)) && (isshoot == true))
+            {
+                isshoot = false;
+                audioSource.PlayOneShot(fireSound);
+                muzzleFlash.Play();
+                Shell.Play();
+                Invoke("ShootTrue", reloadTime);
+                RaycastHit hit;
+                if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, distance))
+                {
+                    if (hit.transform.GetComponent<Rigidbody>())
+                    {
+                        hit.transform.GetComponent<Rigidbody>().AddForceAtPosition(shootPoint.forward * force, hit.point);
+                    }
+                }
+                GameObject myBullet = Instantiate(bullet);
+                myBullet.transform.position = shootPoint.position;
+                myBullet.transform.rotation = shootPoint.rotation;
+                Destroy(myBullet, shootFireLifeTime);
+
+            }
+        }
+
+
+    void skill1()
+    {
+        if (Input.GetKeyDown("q") && isSkill1 == true )
+        {
+            Debug.Log("DASD");
+            isSkill1 = false;
+            audioSource.PlayOneShot(fireSound);
             muzzleFlash.Play();
             Shell.Play();
-            Invoke("ShootTrue", reloadTime);
+            Invoke("Skill1_True", 10.0f);
             RaycastHit hit;
             if (Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, distance))
             {
                 if (hit.transform.GetComponent<Rigidbody>())
                 {
                     hit.transform.GetComponent<Rigidbody>().AddForceAtPosition(shootPoint.forward * force, hit.point);
-                }					
+                }
             }
-
-            GameObject myBullet = Instantiate(bullet);
-            myBullet.transform.position = shootPoint.position;
-            myBullet.transform.rotation = shootPoint.rotation;
-            Destroy(myBullet, shootFireLifeTime);
+            GameObject sk_bullet = Instantiate(skill1_bullet);
+            sk_bullet.transform.position = shootPoint.position;
+            sk_bullet.transform.rotation = shootPoint.rotation;
+            Destroy(sk_bullet, shootFireLifeTime);
 
         }
     }
 
+
     void ShootTrue() 
     {
         isshoot = true;
+    }
+
+    void Skill1_True()
+    {
+        isSkill1 = true;
     }
 }
