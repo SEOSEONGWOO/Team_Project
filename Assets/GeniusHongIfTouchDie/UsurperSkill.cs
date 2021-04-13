67fe7d74e95eada9866a423277f1ba88889d11cc
jhb <tmvlfflrm@gmail.com>
2021-04-13 16:31:15 +0900

    hong

---
+++ Assets/GeniusHongIfTouchDie/UsurperSkill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsurperSkill : MonoBehaviour
+{
+    int DrgHP = 100000;
+    int Sk1Da = 30;
+    int Sk2Da = 15;
+    int Sk3Da = 10;
+    int Sk4Da = 5;
+
+    float Sk1De = 60f;
+    float Sk2De = 30f;
+    float Sk3De = 10f;
+    float Sk4De = 3f;
+
+    int PhaseC = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
