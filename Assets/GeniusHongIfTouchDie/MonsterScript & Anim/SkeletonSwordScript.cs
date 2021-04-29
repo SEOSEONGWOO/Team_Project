b2c9ac4042e0a163a1e7ff8fb0d7b20a40ebc6c1
jhb <tmvlfflrm@gmail.com>
2021-04-29 12:47:03 +0900

    hongsexy

---
+++ Assets/GeniusHongIfTouchDie/MonsterScript & Anim/SkeletonSwordScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonSwordScript : MonoBehaviour
+{
+    public GameObject Gobj;
+
+    int Damage = 0;
+
+    void Start()
+    {
+        Gobj = GameObject.Find("HPCharacter");
+    }
+
+    void Update()
+    {
+        if(SkeletonScript.SkillOn == 1)
+        {
+            gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            Damage = SkeletonScript.SkeletonDamage;
+        }
+        else if(SkeletonScript.SkillOn == 2)
+        {
+            gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            Damage = (int)(SkeletonScript.SkeletonDamage * 1.5);
+        }
+        else if(SkeletonScript.SkillOn == 0)
+        {
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+            Damage = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Gobj.GetComponent<Player_Scr>().GunnerHitFunc(Damage);
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+    }
+}
