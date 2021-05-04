77aea3bcd3166c17fb81b7eb2e4df9c95b5c837e
Kwoncm <81894536+Kwoncm@users.noreply.github.com>
2021-05-04 16:07:23 +0900

    수정

---
+++ Assets/KcmUI/StartText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StartText : MonoBehaviour
+{
+    public GameObject textPanel;
+
+    public float TextTime = 0.0f;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Player")
+        {
+            textPanel.SetActive(true);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextTime += Time.deltaTime;
+        if (TextTime > 4)
+        {
+            textPanel.SetActive(false);
+            TextTime = 0;
+        }
+    }
+}
