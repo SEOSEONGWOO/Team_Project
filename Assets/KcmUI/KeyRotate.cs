40ff0cebd621b765e7484cff7a41afe2a2435022
Kwoncm <81894536+Kwoncm@users.noreply.github.com>
2021-05-16 04:02:02 +0900

    수정

---
+++ Assets/KcmUI/KeyRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRotate : MonoBehaviour
+{
+    public float rotaionSpeed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0, rotaionSpeed * Time.deltaTime));
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("열쇠 획득");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/KcmUI/SkillPanelMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPanelMove : MonoBehaviour
+{
+    public GameObject SkillPanel1;
+    public GameObject SkillPanel2;
+    public GameObject SkillPanel3;
+
+
+    public int SkillPanelChange = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+         {
+            SkillPanelChange -= 1;
+         }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SkillPanelChange += 1;
+        }
+        if(SkillPanelChange == 1)
+        {
+            SkillPanel1.SetActive(true);
+            SkillPanel2.SetActive(false);
+            SkillPanel3.SetActive(false);
+        }
+        if (SkillPanelChange == 2)
+        {
+            SkillPanel1.SetActive(false);
+            SkillPanel2.SetActive(true);
+            SkillPanel3.SetActive(false);
+        }
+        if (SkillPanelChange == 3)
+        {
+            SkillPanel1.SetActive(false);
+            SkillPanel2.SetActive(false);
+            SkillPanel3.SetActive(true);
+        }
+        if(SkillPanelChange > 3)
+        {
+            SkillPanelChange = 1;
+        }
+        if(SkillPanelChange < 1)
+        {
+            SkillPanelChange = 3;
+        }
+    }
+}
