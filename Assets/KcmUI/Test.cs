c96ecdc7e89eaacc596ded4606385af16ee290d8
Kwoncm <81894536+Kwoncm@users.noreply.github.com>
2021-05-17 17:28:57 +0900

    수정

---
+++ Assets/KcmUI/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene("trap test");
+        }
+    }
+}
