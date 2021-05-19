dcd0778df40e2583346136375c54067fdb98fab6
Kwoncm <81894536+Kwoncm@users.noreply.github.com>
2021-05-19 21:17:15 +0900

    상점만들어둠 G키

---
+++ Assets/KcmUI/ShopKey.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKey : MonoBehaviour
+{
+    public GameObject Question;
+    public GameObject panel;
+
+    public bool panelonoff = false;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Question.SetActive(true);
+          
+            if(Question == true)
+            {
+                if(Input.GetKeyDown(KeyCode.G))
+                {
+                    panelonoff = !panelonoff;
+                    panel.SetActive(panelonoff);
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Question.SetActive(false);
+            panel.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Question.SetActive(false);
+        panel.SetActive(panelonoff);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
