fca04cc6399f15b328d3576eda587e898fd1e66f
N-HARU <81941141+N-HARU@users.noreply.github.com>
2021-05-20 11:50:37 +0900

    로비수정2

---
+++ Assets/AKH/Script/Id_pass_test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Id_pass_test : MonoBehaviour
+{
+    public InputField NicknameInput;    //유저 닉
+    public InputField PasswordInput;    //유저 password
+    // Start is called before the first frame update
+    void Start()
+    {
+        //테스트 id,pass
+        NicknameInput.text = "moo@naver.com";
+        PasswordInput.text = "123456789zz";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
