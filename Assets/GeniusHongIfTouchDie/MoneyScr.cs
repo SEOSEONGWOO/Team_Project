04ffc8741ff6e9f051563c74d512a7ff844372e4
jhb <tmvlfflrm@gmail.com>
2021-05-25 11:40:34 +0900

    dd

---
+++ Assets/GeniusHongIfTouchDie/MoneyScr.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyScr : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player_Scr.PlayerMoney = Player_Scr.PlayerMoney + Random.Range(50, 200);
+            Destroy(gameObject);
+        }
+    }
+}
