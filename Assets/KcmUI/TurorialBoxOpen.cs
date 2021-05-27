c0531b6fe99424d0776bfb8d434e1bd18d9592a9
Kwoncm <81894536+Kwoncm@users.noreply.github.com>
2021-05-27 10:58:32 +0900

    튜토리얼박스UI추가

---
+++ Assets/KcmUI/TurorialBoxOpen.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurorialBoxOpen : MonoBehaviour
+{
+    public GameObject boxopenQuestion;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            boxopenQuestion.SetActive(true);
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            boxopenQuestion.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
