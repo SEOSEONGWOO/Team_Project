f7bab0ba17cfc7d2166084b7cebe7870440190b5
SEOSEONGWOO <jsn0526@naver.com>
2021-06-05 22:51:34 +0900

    상점 ㅅ수정

--- Assets/Item/Shop.cs
+++ Assets/Item/Shop.cs
@@ -12,16 +12,29 @@ public class Shop : MonoBehaviour
     public GameObject def_scoll;
     public GameObject hp_potion;
     public GameObject mp_potion;
-    public GameObject Holly_Ball;
+    public GameObject holly_Ball;
 
     public void ATK_scoll_get()
     {
         theInventory.AcquireItem(atk_scoll.GetComponent<ItemPickUp>().item, 1);
         Debug.Log("아이템 구매");
     }
-    void ATK1_scoll_get()
+    public void DEF_scoll_get()
     {
-        theInventory.AcquireItem(atk_scoll.GetComponent<ItemPickUp>().item, 1);
+        theInventory.AcquireItem(def_scoll.GetComponent<ItemPickUp>().item, 1);
+    }
+    public void HP_Potion_get()
+    {
+        theInventory.AcquireItem(hp_potion.GetComponent<ItemPickUp>().item, 1);
+    }
+    public void MP_Potion_get()
+    {
+        theInventory.AcquireItem(mp_potion.GetComponent<ItemPickUp>().item, 1);
+    }
+    public void Holly_ball()
+    {
+        theInventory.AcquireItem(holly_Ball.GetComponent<ItemPickUp>().item, 1);
+        SkillPanelMove.holly = true;
     }
     void Start()
     {
