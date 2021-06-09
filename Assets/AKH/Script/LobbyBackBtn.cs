9660f251651e6b2926810a669024fdcb16137cca
N-HARU <81941141+N-HARU@users.noreply.github.com>
2021-06-09 13:33:10 +0900

    랭킹 버튼

---
+++ Assets/AKH/Script/LobbyBackBtn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyBackBtn : MonoBehaviour
+{
+    public void LobbyBtn()
+    {
+        Debug.Log("로비");
+        SceneManager.LoadScene("DB_Test");
+    }
+    public void Exit()
+    {
+        Debug.Log("종료");
+        Application.Quit();
+    }
+}
